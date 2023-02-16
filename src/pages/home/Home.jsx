cd6941aab7e06157a833be32f66209761669dcf9
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-16 20:05:46 +0530

    Carousel Part Completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./style.scss";
 import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
+
+
+
+
 
 const Home = () => {
   return (
     <div className='homePage'>
-    Home Page
       <HeroBanner /> 
+      <Trending />
     </div>
   )
 }
