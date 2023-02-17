37f2633def7cf215075582f6172d1f4ab3445381
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-18 02:21:22 +0530

    Error Solved

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/PageNotFound";
+import PageNotFound from "../src/pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
