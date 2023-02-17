2cf91223a7386a24c6c769f4c22b6e93b1de1b46
Ranjeet Singh <107486727+Rnjeetsingh@users.noreply.github.com>
2023-02-17 17:55:49 +0530

    VideoSeaction Completed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,8 @@ import useFetch from '../../hooks/useFetch';
 import "./style.scss";
 import { useParams } from 'react-router-dom';
 import DetailsBanner from './detailsBanner/DetailsBanner';
+import Cast from './cast/Cast';
+import VideosSection from './videosSection/VideosSection';
 
 const Details = () => {
   
@@ -16,6 +18,8 @@ const {data: credits, loading: creditsLoading} = useFetch(`/${mediaType}/${id}/c
   return ( 
     <div>
        <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+       <Cast data={credits?.cast} loading={creditsLoading} />
+       <VideosSection data={data} loading={loading} />
     </div>
   )
 }
